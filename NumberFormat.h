69336a756ec7653254bac7b7267fc0d63a8196c2
CarbonTwelve.Developer <CarbonTwelve.Developer@a0245358-5b9e-171e-63e1-2316ddff5996>
2010-11-14 10:40:08 +0000

    Patch fix for GetNumberFormatEx error in Windows XP.

--- NumberFormat.h
+++ NumberFormat.h
@@ -6,9 +6,9 @@ public:
 	CNumberFormat();
 	~CNumberFormat();
 
-	bool LoadDefault(LPCWSTR lpLocaleName = LOCALE_NAME_USER_DEFAULT);
-	int FormatNumberByLocale(LPCTSTR pszNumber, CString &strFormatted, LPCWSTR lpLocaleName = LOCALE_NAME_USER_DEFAULT);
-	int FormatNumberByLocale(double dNumber, CString &strFormatted, LPCWSTR lpLocaleName = LOCALE_NAME_USER_DEFAULT);
+	bool LoadDefault(LCID locale = LOCALE_USER_DEFAULT);
+	int FormatNumberByLocale(LPCTSTR pszNumber, CString &strFormatted, LCID locale = LOCALE_USER_DEFAULT);
+	int FormatNumberByLocale(double dNumber, CString &strFormatted, LCID locale = LOCALE_USER_DEFAULT);
 
 	void SetNumDigits(UINT numDigits) { m_format.NumDigits = numDigits; }
 
